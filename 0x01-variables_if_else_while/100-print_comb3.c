574c480577c9bd69dd4f8fad121de7199d944374
NkunjaBrian <briansouljah@gmail.com>
2023-07-14 12:28:11 -0700

    Write a program that prints all possible different combinations of two digits.

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,27 +1,36 @@
+/*
+ * File: 10-print_comb2.c
+ * Auth: Brennan D Baraban
+ */
+
 #include <stdio.h>
 
 /**
- * main- Entry point
- * @void: Null value
+ * main - Prints the numbers from 00 to 99, numbers separated by
+ *        a comma followed by a space, in ascending order.
  *
- * Description: Print numbers from 00 to 99
- * Return: Zero value
+ * Return: Always 0.
  */
-
 int main(void)
 {
-	int n;
+	int digit1, digit2;
 
-	for (n = 0; n <= 99; n++)
+	for (digit1 = 0; digit1 < 10; digit1++)
 	{
-		putchar((n / 10) + '0');
-		putchar((n % 10) + '0');
-		if (n != 99)
+		for (digit2 = 0; digit2 < 10; digit2++)
 		{
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
+
+			if (digit1 == 9 && digit2 == 9)
+				continue;
+			
 			putchar(',');
 			putchar(' ');
 		}
 	}
+
 	putchar('\n');
+
 	return (0);
 }
