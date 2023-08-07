e863f020c56f87bb18d3cd0c9bd089f516ae4c5d
NkunjaBrian <briansouljah@gmail.com>
2023-08-07 12:32:22 -0700

    Write a function that concatenates two strings.

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,47 +1,42 @@
 #include "main.h"
 #include <stdlib.h>
-/*
- * *str_concat - function to concanate strings
- * @s1 - string to be conc
- * @s2 - string to be concanated
- * return null on failure
+/**
+ * str_concat - get ends of input and add together for size
+ * @s1: input one to concat
+ * @s2: input two to concat
+ * Return: concat of s1 and s2
  */
- char *str_concat(char *s1, char *s2)
+char *str_concat(char *s1, char *s2)
 {
 	char *conct;
 	int i, ci;
 
-	if(s1 == NULL)
-          return "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-	if(s2 == NULL)
-          return "";
+	i = ci = 0;
+	while (s1[i] != '\0')
+		i++;
+	while (s2[ci] != '\0')
+		ci++;
+	conct = malloc(sizeof(char) * (i + ci + 1));
 
-        while(s1[i]!='\0')
-	i++;
+	if (conct == NULL)
+		return (NULL);
+	i = ci = 0;
+	while (s1[i] != '\0')
+	{
+		conct[i] = s1[i];
+		i++;
+	}
 
-        while(s2[ci]!='\0')
-	ci++;
-
-     conct = malloc(sizeof(char)*(i+ci+1));
-	     if(conct == NULL)
-             return NULL;
-
-    i = ci = 0;
-     while(s1[i]!='\0')
-          {
-         conct[i] = s1[i];
-          i++;
-	  }
-	    
-	 while(s2[ci]!='\0')
-	  {
-	  conct[i] = s2[ci];
-	  ci++,i++;
-	  }
-
-	  conct[i] ='\0';
-	  return (conct);
+	while (s2[ci] != '\0')
+	{
+		conct[i] = s2[ci];
+		i++, ci++;
+	}
+	conct[i] = '\0';
+	return (conct);
 }
-
- 	
