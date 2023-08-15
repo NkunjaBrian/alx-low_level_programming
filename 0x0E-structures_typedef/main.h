72f59df054f5e32221633522d29664d30a47d786
NkunjaBrian <briansouljah@gmail.com>
2023-08-15 13:25:39 -0700

    Write a function that creates a new dog.

--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -1,13 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-typedef struct dog
-{
-    char *name;
-    float age;
-    char *owner;
-} dog_t;
-
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
